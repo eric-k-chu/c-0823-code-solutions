469323394747783117c97e5d470e27d2dae52f79
eric-k-chu <kiyuen88ec@gmail.com>
2023-08-29 17:35:17 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,72 @@
+const orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
+    total: 34.0,
+    shipTo: 'JS Masher',
+    orderDelivered: 'Aug 4, 2020',
+    hasDeliveryMsg: false,
+    items: [
+      {
+        name: 'JavaScript for impatient programmers',
+        creator: 'Rauschmayer, Dr. Axel',
+        returnDate: 'Sep 7, 2020',
+        cost: 31.55,
+      },
+    ],
+  },
+  {
+    orderNumber: '113-9974268-1280257',
+    orderPlaced: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderDelivered: 'Jul 20, 2020',
+    hasDeliveryMsg: true,
+    items: [
+      {
+        name: 'The Timeless Way of Building',
+        creator: 'Alexander, Christopher',
+        returnDate: 'Aug 19, 2020',
+        cost: 41.33,
+      },
+    ],
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderDelivered: 'Jul 7, 2020',
+    hasDeliveryMsg: true,
+    items: [
+      {
+        name: 'Gamecube',
+        creator: '',
+        returnDate: 'Aug 5, 2020',
+        cost: 15.98,
+      },
+    ],
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlaced: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderDelivered: 'Jul 5, 2020',
+    hasDeliveryMsg: false,
+    items: [
+      {
+        name: 'GameCube Controller',
+        creator: '',
+        returnDate: 'Aug 4, 2020',
+        cost: 94.95,
+      },
+      {
+        name: 'The Art of Sql',
+        creator: 'Faroult, Stephane',
+        returnDate: 'Aug 4, 2020',
+        cost: 33.99,
+      },
+    ],
+  },
+];
