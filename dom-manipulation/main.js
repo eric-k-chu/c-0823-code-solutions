400258b78f559c24744aa29270a0f2a6065ec0ca
eric-k-chu <kiyuen88ec@gmail.com>
2023-09-11 22:29:41 +0000

    Completed dom-manipulation exercise

---
+++ dom-manipulation/main.js
@@ -0,0 +1,26 @@
+let numOfClicks = 0;
+const $buttonHot = document.querySelector('.hot-button');
+const $clickCount = document.querySelector('.click-count');
+
+function incrementClick() {
+  numOfClicks++;
+  $clickCount.textContent = 'Clicks: ' + numOfClicks;
+}
+$buttonHot.addEventListener('click', incrementClick);
+
+function updateClassName(event) {
+  if (numOfClicks < 4) {
+    event.target.className = 'hot-button cold';
+  } else if (numOfClicks < 7) {
+    event.target.className = 'hot-button cool';
+  } else if (numOfClicks < 10) {
+    event.target.className = 'hot-button tepid';
+  } else if (numOfClicks < 13) {
+    event.target.className = 'hot-button warm';
+  } else if (numOfClicks < 16) {
+    event.target.className = 'hot-button hot';
+  } else {
+    event.target.className = 'hot-button nuclear';
+  }
+}
+$buttonHot.addEventListener('click', updateClassName);
