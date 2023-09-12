78529b1826d477c4b6a79eaee48ffb77bdb347be
eric-k-chu <kiyuen88ec@gmail.com>
2023-09-12 22:05:38 +0000

    Completed javascript-typing-tutor

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,20 @@
+const $letters = document.querySelectorAll('span');
+
+let currentIndex = 0;
+
+function handleKeyDown(event) {
+  if (currentIndex < $letters.length) {
+    if ($letters[currentIndex].textContent === event.key) {
+      $letters[currentIndex].className = 'text-green';
+      currentIndex++;
+
+      if (currentIndex < $letters.length) {
+        $letters[currentIndex].className = 'underline';
+      }
+    } else {
+      $letters[currentIndex].className = 'underline text-red';
+    }
+  }
+}
+
+$letters.forEach(addEventListener('keydown', handleKeyDown));
