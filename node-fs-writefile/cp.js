a914fa2396ef1d89b6c3df43e85a82fb5ecb68eb
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-18 20:29:13 +0000

    Changed cp js to copy all file types

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -3,8 +3,8 @@ import fs from 'node:fs/promises';
 const [, , f1, f2] = process.argv;
 
 async function copyToFile() {
-  const f1Text = await fs.readFile(f1, 'utf8');
-  await fs.writeFile(f2, f1Text, { flag: 'a+' });
+  const f1Content = await fs.readFile(f1);
+  await fs.writeFile(f2, f1Content);
 }
 
 try {
