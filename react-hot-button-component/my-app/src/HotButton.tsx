1776b43747c969ba1e23623dc0c3fc59331d66b0
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-26 22:38:02 +0000

    passing count as a prop to hotbutton

--- react-hot-button-component/my-app/src/HotButton.tsx
+++ react-hot-button-component/my-app/src/HotButton.tsx
@@ -1,12 +1,32 @@
+import './HotButton.css';
+
 type Props = {
-  color: string;
+  count: number;
   onClick: () => void;
 };
 
-export function HotButton({ onClick, color }: Props) {
-  const style = `${color} p-4 rounded-md border-2 border-black hover:border-amber-400 font-semibold`;
+export function HotButton({ count, onClick }: Props) {
+  let color;
+  if (count < 3) {
+    color = 'hot';
+  } else if (count < 6) {
+    color = 'hot-3';
+  } else if (count < 9) {
+    color = 'hot-6';
+  } else if (count < 12) {
+    color = 'hot-9';
+  } else if (count < 15) {
+    color = 'hot-12';
+  } else if (count < 18) {
+    color = 'hot-15';
+  } else {
+    color = 'hot-18';
+  }
+
   return (
-    <button className={style} onClick={() => onClick()}>
+    <button
+      className={`${color} p-4 rounded-md border-2 border-black hover:border-amber-400 font-semibold`}
+      onClick={() => onClick()}>
       Hot Button
     </button>
   );
