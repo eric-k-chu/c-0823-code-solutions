b7fc2b84d20745ce464b2ef6810249aa22efe6f2
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-27 17:14:43 +0000

    completed react toggle switch component

---
+++ react-toggle-switch-component/app-name/src/ToggleSwitch.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export function ToggleSwitch() {
+  const [isOn, setIsOn] = useState(false);
+
+  function handleToggleOn() {
+    setIsOn(() => !isOn);
+  }
+
+  const on =
+    'light flex justify-center items-center container mx-auto h-screen gap-8';
+  const off =
+    'dark flex justify-center items-center container mx-auto h-screen gap-8';
+
+  return (
+    <div className={isOn ? on : off}>
+      <button
+        onClick={handleToggleOn}
+        className="bg-green-500 dark:bg-zinc-200 w-16 h-8 rounded-3xl text-black flex flex-col justify-center border-2 border-black">
+        <div className="bg-white w-7 h-7 rounded-full border-2 border-black self-end dark:self-start" />
+      </button>
+      {isOn ? 'On' : 'Off'}
+    </div>
+  );
+}
