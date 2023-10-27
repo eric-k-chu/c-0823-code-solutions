e4cf7ba39a551aab1c0c6a11c5478ea1f9fc485b
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-27 19:02:09 +0000

    moved pokedex init right below import statements in app.tsx

--- react-rendering-lists/app-name/src/App.tsx
+++ react-rendering-lists/app-name/src/App.tsx
@@ -1,14 +1,14 @@
 import { PokemonList, PokeEntry } from './PokemonList';
 
-function App() {
-  const pokedex: PokeEntry[] = [
-    { number: '001', name: 'Bulbasaur' },
-    { number: '004', name: 'Charmander' },
-    { number: '007', name: 'Squirtle' },
-    { number: '025', name: 'Pikachu' },
-    { number: '039', name: 'Jigglypuff' },
-  ];
+const pokedex: PokeEntry[] = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' },
+];
 
+function App() {
   return (
     <div className="flex justify-center items-center container h-screen mx-auto">
       <PokemonList pokedex={pokedex} />
