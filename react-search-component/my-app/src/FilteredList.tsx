9d63738efbb3714fae54b534c90c8ef9b0c4eb69
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-30 19:20:03 +0000

    WIP

---
+++ react-search-component/my-app/src/FilteredList.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  list: string[];
+  query: string;
+};
+
+export function FilteredList({ list, query }: Props) {
+  const filteredList = list.filter((n) => n.includes(query));
+
+  return (
+    <ul className={filteredList.length > 0 ? 'list-disc p-4' : 'p-4'}>
+      {filteredList.length > 0 ? (
+        filteredList.map((n, i) => <li key={i}>{n}</li>)
+      ) : (
+        <li key={0}>No Items match the filter</li>
+      )}
+    </ul>
+  );
+}
