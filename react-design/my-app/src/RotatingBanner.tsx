d50cd672b244c08c3b1d29a6b3009f0eb32869aa
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-30 17:21:00 +0000

    completed react-design

--- react-design/my-app/src/RotatingBanner.tsx
+++ react-design/my-app/src/RotatingBanner.tsx
@@ -1,16 +1,40 @@
-import { RotatingIndicator } from './RotatingIndicator';
+import { useState } from 'react';
+import { Indicators } from './Indicators';
+import { Button } from './Button';
 
 type Props = {
   items: string[];
 };
 
 export function RotatingBanner({ items }: Props) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  function handleIncrementIndex(): void {
+    setCurrentIndex((currentIndex + 1) % items.length);
+  }
+
+  function handleDecrementIndex(): void {
+    if (currentIndex === 0) {
+      setCurrentIndex(items.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  }
+
+  function handleSetIndex(index: number): void {
+    setCurrentIndex(index);
+  }
+
   return (
-    <div className="flex flex-col gap-y-2 items-center w-full">
-      <h2 className="text-lg font-semibold">{items[0]}</h2>
-      <button className="w-16 flex justify-center">Prev</button>
-      <RotatingIndicator items={items} />
-      <button className="w-16 flex justify-center">Next</button>
+    <div className="flex flex-col gap-y-4 items-center w-full">
+      <h2 className="text-lg font-semibold">{items[currentIndex]}</h2>
+      <Button text="Prev" onClick={handleDecrementIndex} />
+      <Indicators
+        items={items}
+        currentIndex={currentIndex}
+        onClick={handleSetIndex}
+      />
+      <Button text="Next" onClick={handleIncrementIndex} />
     </div>
   );
 }
