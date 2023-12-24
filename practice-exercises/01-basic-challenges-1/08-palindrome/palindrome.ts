4500330ec93bd472679f9d5da4d4b7f9d4032612
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-24 20:17:53 +0000

    completed 01-8/palindrome

--- practice-exercises/01-basic-challenges-1/08-palindrome/palindrome.ts
+++ practice-exercises/01-basic-challenges-1/08-palindrome/palindrome.ts
@@ -1,3 +1,13 @@
 export function isPalindrome(str: string): boolean {
-  return false;
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLocaleLowerCase();
+  let i = 0;
+  let k = cleanStr.length - 1;
+  while (i < k) {
+    if (cleanStr[i] !== cleanStr[k]) {
+      return false;
+    }
+    i++;
+    k--;
+  }
+  return true;
 }
