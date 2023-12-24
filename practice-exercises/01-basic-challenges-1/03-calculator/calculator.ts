19e408dca2c3a4d2ce65cd3e74e4df5dd2177661
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-24 19:50:06 +0000

    completed 01-3/calculator

--- practice-exercises/01-basic-challenges-1/03-calculator/calculator.ts
+++ practice-exercises/01-basic-challenges-1/03-calculator/calculator.ts
@@ -3,5 +3,15 @@ export function calculator(
   num2: number,
   operator: string
 ): number {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num1 / num2;
+  }
   return 0;
 }
