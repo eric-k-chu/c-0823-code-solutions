74075bdee04139112cfda5cde1e5a31597641ad0
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 01:57:16 +0000

    completed 02-2/array-intersection

--- practice-exercises/02-basic-challenges-2/02-array-intersection/array-intersection.ts
+++ practice-exercises/02-basic-challenges-2/02-array-intersection/array-intersection.ts
@@ -1,3 +1,8 @@
 export function arrayIntersection(arr1: unknown[], arr2: unknown[]): unknown[] {
-  return [];
+  const set = new Set(arr1);
+  const arr = [];
+  for (const el of arr2) {
+    if (set.has(el)) arr.push(el);
+  }
+  return arr;
 }
