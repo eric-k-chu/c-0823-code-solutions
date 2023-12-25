adeb0977936285886766f9e440b302a429e4c58b
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 19:41:43 +0000

    completed 03-4/highest-scoring-word

--- practice-exercises/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.ts
+++ practice-exercises/03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.ts
@@ -1,3 +1,16 @@
 export function highestScoringWord(str: string): string {
-  return '';
+  const words = str.split(' ');
+  let max = 0;
+  let highestScoringWord = '';
+
+  for (const word of words) {
+    const score = word
+      .split('')
+      .reduce((acc, n) => acc + n.charCodeAt(0) - 'a'.charCodeAt(0), 0);
+    if (max < score) {
+      max = score;
+      highestScoringWord = word;
+    }
+  }
+  return highestScoringWord;
 }
