5783f6f50d0723a75a25fc3158a760cd4c38d483
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 03:27:11 +0000

    completed 02-9/format-phone-number

--- practice-exercises/02-basic-challenges-2/09-format-phone-number/format-phone-number.ts
+++ practice-exercises/02-basic-challenges-2/09-format-phone-number/format-phone-number.ts
@@ -1,3 +1,6 @@
 export function formatPhoneNumber(numbers: number[]): string {
-  return '';
+  const areaCode = numbers.slice(0, 3).join('');
+  const middle = numbers.slice(3, 6).join('');
+  const end = numbers.slice(6).join('');
+  return `(${areaCode}) ${middle}-${end}`;
 }
