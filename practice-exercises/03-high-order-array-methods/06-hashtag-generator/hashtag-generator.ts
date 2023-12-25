8ef6db48926233a30ccc61c12a01f0a5fd4d5bec
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 20:03:21 +0000

    completed 03-6/hashtag-generator

--- practice-exercises/03-high-order-array-methods/06-hashtag-generator/hashtag-generator.ts
+++ practice-exercises/03-high-order-array-methods/06-hashtag-generator/hashtag-generator.ts
@@ -1,3 +1,11 @@
 export function generateHashtag(str: string): string | undefined {
-  return undefined;
+  if (str.length < 1 || str.length > 140) return undefined;
+
+  const pascalStr = str
+    .split(' ')
+    .filter((n) => n.length > 1)
+    .map((n) => n[0].toUpperCase() + n.slice(1))
+    .join('');
+
+  return `#${pascalStr}`;
 }
