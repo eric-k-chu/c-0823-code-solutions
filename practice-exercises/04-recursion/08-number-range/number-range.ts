2481ec681bd8114b4fda2874256e31cc50aeed9b
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 21:13:33 +0000

    completed 04-8/number-range

--- practice-exercises/04-recursion/08-number-range/number-range.ts
+++ practice-exercises/04-recursion/08-number-range/number-range.ts
@@ -1,3 +1,4 @@
 export function numberRange(startNum: number, endNum: number): number[] {
-  return [];
+  if (startNum === endNum) return [startNum];
+  return [startNum, ...numberRange(startNum + 1, endNum)];
 }
