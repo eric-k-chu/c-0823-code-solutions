91024a15bf7734e6bd39f84883ee0a7324184313
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 20:35:57 +0000

    completed 03-9/password-validator

--- practice-exercises/03-high-order-array-methods/09-password-validator/password-validator.ts
+++ practice-exercises/03-high-order-array-methods/09-password-validator/password-validator.ts
@@ -1,3 +1,6 @@
 export function validatePassword(password: string): boolean {
-  return false;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasDigit = /\d/.test(password);
+  return password.length >= 8 && hasUpperCase && hasLowerCase && hasDigit;
 }
