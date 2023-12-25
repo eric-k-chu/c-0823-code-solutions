80c9b932baf178d850e552bffdcf7f7a64331ba1
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 03:01:10 +0000

    completed 02-7/first-non-repeating

--- practice-exercises/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.ts
+++ practice-exercises/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.ts
@@ -1,5 +1,14 @@
 export function findFirstNonRepeatingCharacter(
   str: string
 ): string | undefined {
+  const counts: Record<string, number> = {};
+  for (let i = 0; i < str.length; i++) {
+    counts[str[i]] = (counts[str[i]] || 0) + 1;
+  }
+
+  for (const char in counts) {
+    if (counts[char] === 1) return char;
+  }
+
   return undefined;
 }
