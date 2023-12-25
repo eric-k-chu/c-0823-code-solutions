f1b15895371eed4c515c7d72209b9d0d12f9a136
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 20:21:57 +0000

    completed 03-7/valid-ipv4

--- practice-exercises/03-high-order-array-methods/07-valid-ipv4/valid-ipv4.ts
+++ practice-exercises/03-high-order-array-methods/07-valid-ipv4/valid-ipv4.ts
@@ -1,3 +1,15 @@
 export function isValidIPv4(input: string): boolean {
-  return false;
+  const octets = input.split('.');
+
+  if (octets.length !== 4) return false;
+
+  return octets.every((octet) => {
+    if (/^\d+$/.test(octet) === false) return false;
+
+    if (octet.length > 1 && octet[0] === '0') return false;
+
+    if (+octet < 0 || +octet > 255) return false;
+
+    return true;
+  });
 }
