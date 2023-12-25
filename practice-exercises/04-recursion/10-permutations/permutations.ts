3807588bc4b1937b0e56c123072e46a2a4b2bb4d
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-25 22:42:27 +0000

    completed 04-10/permutations

--- practice-exercises/04-recursion/10-permutations/permutations.ts
+++ practice-exercises/04-recursion/10-permutations/permutations.ts
@@ -1,3 +1,21 @@
 export function permutations(str: string): string[] {
-  return [];
+  const perms: string[] = [];
+
+  if (str.length === 0) {
+    perms.push('');
+    return perms;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const first = str[i];
+    const rest = str.slice(0, i) + str.slice(i + 1);
+
+    const restPerms = permutations(rest);
+
+    for (const perm of restPerms) {
+      perms.push(first + perm);
+    }
+  }
+
+  return perms;
 }
