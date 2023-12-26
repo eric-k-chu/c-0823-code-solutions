eea2c59356f54c6bad540f2f85d5535a70a79152
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-26 09:23:32 +0000

    completed 05-3/constant-time-complexity and 05-4/linear-time-complexity

--- practice-exercises/05-complexity/03-constant-time-complexity/constant-time.ts
+++ practice-exercises/05-complexity/03-constant-time-complexity/constant-time.ts
@@ -3,3 +3,9 @@
 
   Constant time means that the time required to complete a function is the same regardless of the size of the input data set.
 */
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log('Example of constant time: ', add(1, 2));
--- practice-exercises/05-complexity/04-linear-time-complexity/linear-time.ts
+++ practice-exercises/05-complexity/04-linear-time-complexity/linear-time.ts
@@ -4,3 +4,11 @@
   Linear time means that the time required to complete a function
   is directly proportional to the size of the input data set.
 */
+
+function getLength(arr: unknown[]): number {
+  let length = 0;
+  arr.forEach(() => length++);
+  return length;
+}
+
+console.log('Example of linear time: ', getLength([1, 2, 3, 4, 5]));
