a61e7b8e675baec913f23e820732536486e578b5
eric-k-chu <kiyuen88ec@gmail.com>
2023-12-26 09:35:41 +0000

    completed 05-5/quadratic-time

--- practice-exercises/05-complexity/05-quadratic-time-complexity/quadradic-time.ts
+++ practice-exercises/05-complexity/05-quadratic-time-complexity/quadradic-time.ts
@@ -4,3 +4,20 @@ Quadratic Time O(n^2)
 Quadratic time means that the time required to complete a function
 is proportional to the square of the input data set.
 */
+
+function circle(radius: number): void {
+  const centerX = radius;
+  const centerY = radius;
+
+  for (let y = 0; y <= 2 * radius; y++) {
+    let row = '';
+    for (let x = 0; x <= 2 * radius; x++) {
+      const distance = Math.sqrt((x - centerX) ** 2 + (y - centerY) ** 2);
+      row += Math.abs(distance - radius) < 0.5 ? '*' : ' ';
+    }
+    console.log(row);
+  }
+}
+
+console.log('Example of quadratic time:');
+circle(20);
